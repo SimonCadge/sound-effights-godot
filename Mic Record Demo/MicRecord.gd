e309cbebcf9fe997d0165e53f6a9b74271f41fe3
Simon Cadge <SimonHCadge@gmail.com>
2020-06-22 12:38:40 +0100

    Rearrange folder layout

--- Mic Record Demo/MicRecord.gd
+++ Mic Record Demo/MicRecord.gd
@@ -1,27 +1,31 @@
 extends Control
 
 var recorder
-var time_step = MAX_TIMESTEP
+var last_time
 var audio_analyser
 var analysis
 
-const MAX_TIMESTEP = 0.2
+const MAX_TIMESTEP = 0.2 * 1000
 
 func _ready():
 	var recorder_index = AudioServer.get_bus_index("Record")
 	recorder = AudioServer.get_bus_effect(recorder_index, 0)
-	audio_analyser = load("res://AudioAnalyser.gdns").new() # Load an instance of the rust Audio Analyser code.
-	
-func _process(delta):
+	audio_analyser = load("res://Godot Native/AudioAnalyser.gdns").new() # Load an instance of the rust Audio Analyser code.
+
+func _process(_delta):
 	if recorder.is_recording_active():
-		if time_step - delta <= 0:
+		var current_time = OS.get_ticks_msec()
+		var elapsed_time = current_time - last_time
+		if elapsed_time >= MAX_TIMESTEP:
 			var recording = recorder.get_recording()
-			var analysis_result = audio_analyser.cut_and_analyse_audio(recording, MAX_TIMESTEP * 0.9)
+			var analysis_result = audio_analyser.analyse_end_of_audio(recording, elapsed_time / 1000.0)
 			if analysis_result.has("Ok"):
-				print(analysis_result.Ok.frequency)
-			time_step = MAX_TIMESTEP
-		else :
-			time_step -= delta
+				# print(analysis_result.Ok.frequency)
+				print(analysis_result.Ok.volume)
+				# print(analysis_result.Ok.duration)
+			else:
+				print(analysis_result.Err)
+			last_time = current_time
 
 func _on_RecordButton_pressed():
 	if recorder.is_recording_active():
@@ -38,9 +42,8 @@ func _on_RecordButton_pressed():
 	else:
 		$PlayButton.disabled = true
 		recorder.set_recording_active(true)
-		time_step = MAX_TIMESTEP
+		last_time = OS.get_ticks_msec()
 		$RecordButton.text = "Stop"
-
 
 func _on_PlayButton_pressed():
 	$AudioStreamPlayer.stream = analysis.sample
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ struct AudioAnalyser;
 struct Analysis {
     sample: gdnative::AudioStreamSample,
     duration: f32,
+    volume: f32,
     frequency: Option<f32>,
     clarity: Option<f32>,
 }
@@ -28,7 +29,7 @@ impl AudioAnalyser {
     #[export]
     fn trim_and_analyse_audio(&self, _owner: gdnative::Reference, audio_stream: gdnative::AudioStreamSample) -> Result<Analysis, String> {
         // Convert data into a standard format that we can analyse.
-        let audio_data = get_samples_from_audiostream(audio_stream.clone());
+        let audio_data = get_samples_from_audiostream(audio_stream.clone(), 0, 0);
         let audio_data = match audio_data {
             Ok(audio_data) => audio_data,
             Err(error) => return Err(error),
@@ -76,31 +77,31 @@ impl AudioAnalyser {
         };
 
         // If the audio data is stereo there are two samples in the byte array for each one in the f32 vector.
-        let is_stereo = audio_stream.is_stereo();
-        let bytes_per_sample = match is_stereo {
-            true => bytes_per_sample * 2,
-            false => bytes_per_sample,
+        let num_channels = match audio_stream.is_stereo() {
+            true => 2,
+            false => 1,
         };
+
         // Cut out silence at the start and end.
-        let resized_audio_stream = resize_audio(audio_stream, start_index * bytes_per_sample, end_index * bytes_per_sample);
+        let resized_audio_stream = resize_audio(audio_stream, start_index * bytes_per_sample * num_channels, end_index * bytes_per_sample * num_channels);
         let resized_audio_stream = match resized_audio_stream {
             Ok(new_stream) => new_stream,
             Err(error) => return Err(error),
         };
 
-        return get_analysis(resized_audio_stream);
+        return get_analysis(resized_audio_stream, 0, 0);
     }
 
     #[export]
     fn analyse_audio(&self, _owner: gdnative::Reference, audio_stream: gdnative::AudioStreamSample) -> Result<Analysis, String> {
-        return get_analysis(audio_stream);
+        return get_analysis(audio_stream, 0, 0);
     }
 
     #[export]
-    fn cut_and_analyse_audio(&self, _owner: gdnative::Reference, audio_stream: gdnative::AudioStreamSample, duration: f32) -> Result<Analysis, String> {
+    fn analyse_end_of_audio(&self, _owner: gdnative::Reference, audio_stream: gdnative::AudioStreamSample, duration: f32) -> Result<Analysis, String> {
         // Determine how many bytes in the byte array correspond to one f32 sample.
         let audio_format = audio_stream.get_format();
-        let bytes_per_sample = match audio_format {
+        let bytes_per_sample: usize = match audio_format {
             gdnative::AudioStreamSampleFormat::Format8Bits => 1,
             gdnative::AudioStreamSampleFormat::Format16Bits => 2,
             gdnative::AudioStreamSampleFormat::ImaAdpcm => {
@@ -110,40 +111,31 @@ impl AudioAnalyser {
         };
 
         // If the audio data is stereo there are two samples in the byte array for each one in the f32 vector.
-        let is_stereo = audio_stream.is_stereo();
-        let bytes_per_sample = match is_stereo {
-            true => bytes_per_sample * 2,
-            false => bytes_per_sample,
+        let num_channels: usize = match audio_stream.is_stereo() {
+            true => 2,
+            false => 1,
         };
         
         let end_index = audio_stream.get_data().len() as usize;
-        let num_samples = (duration * audio_stream.get_mix_rate() as f32).floor() * bytes_per_sample as f32;
-        let start_index = (end_index as f32 - num_samples) as usize;
+        let num_samples = (duration * audio_stream.get_mix_rate() as f32).floor() as usize;
+        let start_index = end_index - (num_samples * bytes_per_sample * num_channels);
 
-        godot_dbg!(end_index);
-        godot_dbg!(num_samples);
-        godot_dbg!(start_index);
-
-        let resized_audio_stream = resize_audio(audio_stream, start_index, end_index);
-        let resized_audio_stream = match resized_audio_stream {
-            Ok(new_stream) => new_stream,
-            Err(error) => return Err(error),
-        };
-
-        return get_analysis(resized_audio_stream);
+        if start_index > end_index {
+            return get_analysis(audio_stream, 0, 0);
+        } else {
+            return get_analysis(audio_stream, start_index, num_samples);
+        }
     }
 }
 
-fn get_analysis(audio_stream: gdnative::AudioStreamSample) -> Result<Analysis, String> {
+fn get_analysis(audio_stream: gdnative::AudioStreamSample, data_offset: usize, num_samples: usize) -> Result<Analysis, String> {
     // Convert abstract bytes representation into Vector of f32 samples.
-    let audio_samples = get_samples_from_audiostream(audio_stream.clone());
-    let audio_samples = match audio_samples {
-        Ok(audio_data) => audio_data,
-        Err(error) => return Err(error),
-    };
+    let audio_samples = get_samples_from_audiostream(audio_stream.clone(), data_offset, num_samples).unwrap();
 
     // Calculate duration in seconds of recorded audio.
     let duration = audio_samples.len() as f32 / audio_stream.get_mix_rate() as f32;
+
+    let volume = audio_samples.iter().fold(0.0, |accumulator, x| accumulator + x.abs()) / audio_samples.len() as f32;
 
     // Determine pitch of audio sample. If the person isn't singing it might not succeed.
     let pitch = get_pitch_from_audio_samples(audio_samples, audio_stream.get_mix_rate() as usize);
@@ -155,19 +147,20 @@ fn get_analysis(audio_stream: gdnative::AudioStreamSample) -> Result<Analysis, S
     return Ok(Analysis {
         sample: audio_stream,
         duration,
+        volume,
         frequency,
         clarity,
     });
 }
 
-fn get_samples_from_audiostream(audio_stream: gdnative::AudioStreamSample) -> Result<Vec<f32>, String> {
+fn get_samples_from_audiostream(audio_stream: gdnative::AudioStreamSample, data_offset: usize, num_samples: usize) -> Result<Vec<f32>, String> {
     let audio_data = audio_stream.get_data();
     let audio_format = audio_stream.get_format();
 
     // Determine whether a sample is comprised of one or two bytes in the byte array.
     // An adaptive length encoding is supported but I think it's unlikely that audio will be
     // recorded in that format.
-    let bytes_per_sample = match audio_format {
+    let bytes_per_sample: usize = match audio_format {
         gdnative::AudioStreamSampleFormat::Format8Bits => 1,
         gdnative::AudioStreamSampleFormat::Format16Bits => 2,
         gdnative::AudioStreamSampleFormat::ImaAdpcm => {
@@ -177,22 +170,28 @@ fn get_samples_from_audiostream(audio_stream: gdnative::AudioStreamSample) -> Re
     };
 
     // If the audio is in a stereo format then there are two channels interleaved, meaning twice as many samples.
-    let is_stereo = audio_stream.is_stereo();
-    let num_samples: usize = match is_stereo {
-        true => (audio_data.len() as usize / bytes_per_sample) / 2,
-        false => audio_data.len() as usize / bytes_per_sample,
+    let num_channels = match audio_stream.is_stereo() {
+        true => 2,
+        false => 1,
+    };
+
+    let num_samples: usize = if num_samples == 0 {
+        (audio_data.len() as usize - data_offset) / (bytes_per_sample * num_channels)
+    } else {
+        num_samples
     };
 
     // Convert samples into an array of f32 samples.
     let mut audio_samples: Vec<f32> = vec![0.0; num_samples];
-    if is_stereo {
+    if num_channels == 2 {
         // If we've got stereo sound then we average the sound between the two channels.
-        for index in (0..audio_data.len()).step_by(bytes_per_sample * 2) {
+        for samples_index in 0..audio_samples.len() {
+            let data_index = (samples_index * bytes_per_sample * 2 + data_offset) as i32;
             match bytes_per_sample {
                 // Signed 8 bit PCM data ranges from -128 to 127.
-                1 => audio_samples[index as usize / (bytes_per_sample * 2)] = ((audio_data.get(index) as f32 / 128.0) + (audio_data.get(index + 1) as f32 / 128.0)) / 2.0,
+                1 => audio_samples[samples_index] = ((audio_data.get(data_index) as f32 / 128.0) + (audio_data.get(data_index + 1) as f32 / 128.0)) / 2.0,
                 // Signed 16 bit PCM data ranges from -32768 to 32767.
-                2 => audio_samples[index as usize / (bytes_per_sample * 2)] = ((i16::from_le_bytes([audio_data.get(index), audio_data.get(index + 1)]) as f32 / 32768.0) + (i16::from_le_bytes([audio_data.get(index+2), audio_data.get(index + 3)]) as f32 / 32768.0)) / 2.0,
+                2 => audio_samples[samples_index] = ((i16::from_le_bytes([audio_data.get(data_index), audio_data.get(data_index + 1)]) as f32 / 32768.0) + (i16::from_le_bytes([audio_data.get(data_index + 2), audio_data.get(data_index + 3)]) as f32 / 32768.0)) / 2.0,
                 _ => {
                     godot_error!("Unexpected number of bytes per sample {}", bytes_per_sample);
                     return Err(format!("Unexpected number of bytes per sample {}", bytes_per_sample));
@@ -201,10 +200,11 @@ fn get_samples_from_audiostream(audio_stream: gdnative::AudioStreamSample) -> Re
         }
     } else {
         // If we have mono sound then we just convert each sample to f32 format.
-        for index in (0..audio_data.len()).step_by(bytes_per_sample) {
+        for samples_index in 0..audio_samples.len() {
+            let data_index = (samples_index * bytes_per_sample * 2 + data_offset) as i32;
             match bytes_per_sample {
-                1 => audio_samples[index as usize / bytes_per_sample] = audio_data.get(index) as f32 / 128.0,
-                2 => audio_samples[index as usize / bytes_per_sample] = i16::from_le_bytes([audio_data.get(index), audio_data.get(index + 1)]) as f32 / 32768.0,
+                1 => audio_samples[samples_index] = audio_data.get(data_index) as f32 / 128.0,
+                2 => audio_samples[samples_index] = i16::from_le_bytes([audio_data.get(data_index), audio_data.get(data_index + 1)]) as f32 / 32768.0,
                 _ => {
                     godot_error!("Unexpected number of bytes per sample {}", bytes_per_sample);
                     return Err(format!("Unexpected number of bytes per sample {}", bytes_per_sample));
